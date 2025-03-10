4c59614b951231ff2b6b108488130f7f11590697
Shankargeotab <bshankar@dicaninc.com>
2025-03-10 13:52:14 -0400

    Update plowStatusMap.js

--- plowStatusMap.js
+++ plowStatusMap.js
@@ -1,9 +1,9 @@
-window.geotab.addin.plowStatusMap = function (api, state) {
+window.geotab.addin.plowStatusMap = function(api, state) {
     return {
-        initialize: function () {
+        initialize: function() {
             console.log("Plow Status Map Add-in Initialized");
         },
-        focus: function () {
+        focus: function() {
             console.log("Plow Status Map Add-in Focused");
             // Ensure toolbar exists
             let toolbar = document.querySelector(".leaflet-bar");
@@ -21,13 +21,13 @@ window.geotab.addin.plowStatusMap = function (api, state) {
             plowButton.innerText = "Show Plow Status";
             plowButton.style.padding = "5px";
             plowButton.style.cursor = "pointer";
-            plowButton.onclick = function () {
+            plowButton.onclick = function() {
                 state.open("plow_status_addin");
             };
             toolbar.appendChild(plowButton);
             console.log("Plow Status button added to map toolbar!");
         },
-        blur: function () {
+        blur: function() {
             console.log("Plow Status Map Add-in Blurred");
             let plowButton = document.getElementById("plowStatusButton");
             if (plowButton) {
